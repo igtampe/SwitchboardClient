a9c2054eb7c7a39aa8a2dedf772e3a2d6c67cae9
Ignacio Tampe <Igtampe@outlook.com>
2020-07-01 10:47:03 -0400

    C o m m e n t s (also READ Supercommand)

--- Program.cs
+++ Program.cs
@@ -1,55 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Text;
-using System.Threading.Tasks;
 using BasicRender;
 
 namespace SwitchboardClient {
     class Program {
 
+        /// <summary>Prefix for the "Command Prompt"</summary>
         public static String Prefix = "DISCONNECTED";
+
+        /// <summary>The SwitchboardClient object</summary>
         public static Switchboard.SwitchboardClient MainClient;
 
         static void Main(string[] args) {
 
+            //Set title and clear the screan
             Console.Title = "Switchboard Console: Disconnected";
-
             Console.Clear();
 
+            //Draw the header
             SwitchboardLogo Logo = new SwitchboardLogo();
-
             Logo.draw(2,1);
             Render.Sprite("Switchboard Console [Version 1.0]",Console.BackgroundColor,ConsoleColor.White,3 + Logo.GetWidth(),2);
             Render.Sprite("(C)2020 Chopo, No Rights Reserved",Console.BackgroundColor,ConsoleColor.White,3 + Logo.GetWidth(),3);
 
+            //Set the position, and draw a neat little message
             Render.SetPos(0,2 + Logo.GetHeight());
-
             Render.Echo("Welcome to the Switchboard Console! Type CONNECT [IP]:[PORT] to connect to a server! \n\n");
 
+            //The Main Loop
             while(true) {
 
+                //Get input
                 String Prompt = PromptInput();
                 String[] PromptSplit = Prompt.Split(' ');
 
-
+                //Try to locally parse the message
                 switch(PromptSplit[0].ToUpper()) {
                     case "CONNECT":
-                        if(MainClient != null) { Render.Echo("There's already an ongoing connection! Close this one to open another one."); }
-                        else if(PromptSplit.Length!=2) { Render.Echo("Impropper connection request. Try something like 127.0.0.1:909"); } 
+                        if(MainClient != null) { Render.Echo("There's already an ongoing connection! Close this one to open another one."); } //Make sure we're not already connected
+                        else if(PromptSplit.Length!=2) { Render.Echo("Impropper connection request. Try something like 127.0.0.1:909"); } //Make sure the connection prompt is the right length
                         else {
-                            String[] IPPortSplit = PromptSplit[1].Split(':');
-                            MainClient = new Switchboard.SwitchboardClient(IPPortSplit[0],int.Parse(IPPortSplit[1]));
-                            if(MainClient.Connect()) { Prefix = IPPortSplit[0];   Console.Title = "Switchboard Console: " + IPPortSplit[0];} else { MainClient = null; }
+                            String[] IPPortSplit = PromptSplit[1].Split(':'); //Split the IP and port
+                            if(IPPortSplit.Length != 2) { Render.Echo("IP/Port combination not valid"); break; } //Make sure the IP/Port combination is the right length.
+                            
+                            MainClient = new Switchboard.SwitchboardClient(IPPortSplit[0],int.Parse(IPPortSplit[1])); //Create client
+                            if(MainClient.Connect()) { Prefix = IPPortSplit[0];   Console.Title = "Switchboard Console: " + IPPortSplit[0];}  //Initialize it, and if we manage to connect, setup the prefix and title.
+                            else { MainClient = null; } //If not reset mainclient to null.
                         }
                         break;
                     case "CLOSE":
-                        if(MainClient == null) { Render.Echo("No connection to close"); } 
-                        else { MainClient.Close(); Prefix = "DISCONNECTED"; Console.Title = "Switchboard Console: Disconnected"; MainClient = null; }
+                        if(MainClient == null) { Render.Echo("No connection to close"); }  //Make sure we cannot close if there is no connection.
+                        else { MainClient.Close(); Prefix = "DISCONNECTED"; Console.Title = "Switchboard Console: Disconnected"; MainClient = null; } //Close the connection.
+                        break;
+                    case "READ":
+                        if(!MainClient.Available) {
+                            Render.Echo("No data is available! Wait for data? ");
+                            if(!YesNo()) { break; } //Display a warning if there is no data to read, and if the user wants to read the data.
+                        }
+                        Render.Echo(MainClient.Receive()); //Read the data and display it.
                         break;
                     default:
-                        if(MainClient == null || !MainClient.Connected) { Render.Echo("Client is not connected! Connect using CONNECT [IP]:[PORT]"); } 
+
+                        //Try to send the commend to the server.
+                        if(MainClient == null || !MainClient.Connected) { Render.Echo("Client is not connected! Connect using CONNECT [IP]:[PORT]"); }  //warn the user if there's no connection.
                         else { Render.Echo(MainClient.SendReceive(Prompt)); } 
                         break;
                 }
@@ -59,14 +71,18 @@ namespace SwitchboardClient {
 
         }
 
+        /// <summary>Prompts the user for input</summary>
         public static string PromptInput() {
             Render.Echo("\n\n"+Prefix+"> ");
             return Console.ReadLine();
-
         }
 
-
-
+        /// <summary>Prompts the user with Y/N.</summary>
+        /// <returns>True if the user hits Y, false otherwise.</returns>
+        public static bool YesNo() {
+            Render.Echo("(Y/N) ");
+            if(Console.ReadKey().Key==ConsoleKey.Y) { return true; } else { return false; }
+        }
 
     }
 }
--- Switchboard/SwitchboardClient.cs
+++ Switchboard/SwitchboardClient.cs
@@ -6,24 +6,33 @@ using System.Text;
 using System.Threading;
 
 namespace Switchboard {
+
+    //In the event of using this outside the console, probably remove the Basicrender calls.
     public class SwitchboardClient {
-
-        //In the event of using this outside the console, probably remove the Basicrender calls.
-
+        
+        /// <summary>Main TCP Client</summary>
         private TcpClient Client;
+
+        /// <summary>haha River like a larger stream ahahaha</summary>
         private NetworkStream River;
 
+        /// <summary>Whether or not the client is connected to the server.</summary>
         public bool Connected => Client.Connected;
+
+        /// <summary>Whether or not the client has data available to read.</summary>
         public bool Available => River.DataAvailable;
 
+        /// <summary>counter for the animation</summary>
         private static int ConnectionStatus=0;
 
+        /// <summary>IP of the remote server.</summary>
         private readonly String IP;
+
+        /// <summary>Port of the remote server</summary>
         private readonly int Port;
 
-        /// <summary>Generates a Switchboard Client, but Does not start it</summary>
+        /// <summary>Generates a Switchboard Client, but does not start it</summary>
         public SwitchboardClient(String IP, int Port) {
-            Render.Echo("Attempting to connect to " + IP + ":" + Port + " ");
             this.IP = IP;
             this.Port = Port;
             Client = new TcpClient();
@@ -32,6 +41,9 @@ namespace Switchboard {
         /// <summary>Initiate the connection</summary>
         /// <returns>True if it managed to connect, false otherwise</returns>
         public Boolean Connect() {
+
+            //Attempt to connect.
+            Render.Echo("Attempting to connect to " + IP + ":" + Port + " ");
             Client.ConnectAsync(IP,Port);
 
             //15 second time out
@@ -40,18 +52,24 @@ namespace Switchboard {
                 if(Client.Connected) { break; }
                 Thread.Sleep(500);
             }
-
+            
+            //Verify if we've connected.
             if(!Client.Connected) {
                 Render.Sprite("\nERROR: Could not connect to server. Maybe it's busy?",ConsoleColor.Black,ConsoleColor.Red);
                 return false ;
             }
 
+            //Neat we connected!
             Render.Sprite("\nConnected to the server!\n\n",ConsoleColor.Black,ConsoleColor.Green);
             River = Client.GetStream();
+
+            //Also display the welcome message
+            Render.Echo(SendReceive("WELCOME"));
 
             return true;
         }
 
+        /// <summary>Close the Client's connection</summary>
         public void Close() {
             if(!Connected) { return; } //Make sure attempting to close an already closed connection doesn't cause an exception. That's kinda bobo.
             //Send CLOSE to the server, closing that side.
@@ -64,37 +82,37 @@ namespace Switchboard {
         }
 
         /// <summary>Send data to the switchboard server, and retrieve a response</summary>
-        public String SendReceive(String Data) {
-            Send(Data);
-            return Receive();
-        }
+        public String SendReceive(String Data) {Send(Data); return Receive();}
 
         /// <summary>Only send data, do not receive. Maybe could be useful in the future.</summary>
         public void Send(String data) {
             if(!Connected) { throw new InvalidOperationException("This client is not connected right now!"); }
-            Byte[] Bytes = Encoding.ASCII.GetBytes(data);
-            River.Write(Bytes,0,Bytes.Length);
+            Byte[] Bytes = Encoding.ASCII.GetBytes(data); //Convert the string to bytes.
+            River.Write(Bytes,0,Bytes.Length); //send the bytes
         }
 
         /// <summary>Only receive data</summary>
         public String Receive() {
             if(!Connected) { throw new InvalidOperationException("This client is not connected right now!"); }
             while(!Available) {
+
+                //Perhaps here we can also specify a timeout.
+
+                //on a GUI application, here would be a good place to check if your user needs to  cancel the operation. Probably replace my own check
+                if(Console.KeyAvailable) { if(Console.ReadKey().Key == ConsoleKey.Escape) { return "BREAK"; } } //This is to allow a user to breka the connection
                 Thread.Sleep(500);
-                //on a GUI application, here would be a good place to check if your user needs to  cancel the operation.
             }
 
             List<Byte> Bytes = new List<Byte>();
-            while(Available) { Bytes.Add((byte)(River.ReadByte())); }
-            return Encoding.ASCII.GetString(Bytes.ToArray());
-
+            while(Available) { Bytes.Add((byte)(River.ReadByte())); } //Get all the bytes in a nice little array.
+            return Encoding.ASCII.GetString(Bytes.ToArray()); //convert the array of bytes back into a neat little bit of text, and return it.
 
         }
 
         /// <summary>Spinner animation</summary>
         public static void ConnectAnim() {
-            Render.SetPos(Console.CursorLeft-1,Console.CursorTop);
-            String Output = "";
+            Render.SetPos(Console.CursorLeft-1,Console.CursorTop); //Move the cursor a little back
+            String Output = ""; //This will hold our output
             switch(ConnectionStatus) {
                 case 0:
                     Output = "/";
@@ -103,7 +121,7 @@ namespace Switchboard {
                     Output = "-";
                     break;
                 case 2:
-                    Output = "\\";
+                    Output = "\\"; //two because we need to escpae.
                     break;
                 case 3:
                     Output = "|";
@@ -113,8 +131,8 @@ namespace Switchboard {
                     break;
             }
 
-            ConnectionStatus++;
-            Render.Echo(Output);
+            ConnectionStatus++; //Increment Connection Status
+            Render.Echo(Output); //Render the spinner.
         }
 
     }
